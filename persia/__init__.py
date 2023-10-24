df3a27a48e614172eeec1c5c3a61e07b9132c999
whoami <whoami@whoami.org>
2023-10-24 14:41:32 +0800

    add MANIFEST.in; use pyprojcet.toml

--- persia/__init__.py
+++ persia/__init__.py
@@ -1,9 +1,15 @@
-from .version import __version__
+from importlib.metadata import PackageNotFoundError, version
 
+try:
+    __version__ = version("persia")
+except PackageNotFoundError:
+    # package is not installed
+    pass
+
+from persia import ctx as ctx
+from persia import data as data
+from persia import embedding as embedding
 from persia import logger as logger
 from persia import prelude as prelude
-from persia import ctx as ctx
-from persia import embedding as embedding
-from persia import data as data
 from persia import service as service
 from persia import utils as utils
